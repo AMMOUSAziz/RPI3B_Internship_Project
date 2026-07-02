82ba513f6e701e87fb7637d7461310e91dee3194
AMMOUSAziz <96547899+AMMOUSAziz@users.noreply.github.com>
2026-07-02 14:09:43 +0200

    Update dht_simpletest.py

--- dht_simpletest.py
+++ dht_simpletest.py
@@ -1,36 +1,33 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-FileCopyrightText: 2017 Limor Fried for Adafruit Industries
+#
 # SPDX-License-Identifier: MIT
 
 import time
-import board
+
 import adafruit_dht
+import board
 
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(board.D4, use_pulseio=False)
-
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+dht = adafruit_dht.DHT11(board.D2)
 
 while True:
     try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(f"Temp: {temperature_f:.1f} F / {temperature_c:.1f} C    Humidity: {humidity}% ")
+        temperature = dht.temperature
+        humidity = dht.humidity
+        # Print what we got to the REPL
+        toPrint=f"Temp: {temperature} *C \t Humidity: {humidity}%"
+        print(str(toPrint))
+    except RuntimeError as e:
+        # Reading doesn't always work! Just print error and we'll try again
+        print("Reading from DHT failure: ", e.args)
 
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
+    time.sleep(1)
 
-    time.sleep(2.0)
+
+
+
+
+
+
 
 
 
