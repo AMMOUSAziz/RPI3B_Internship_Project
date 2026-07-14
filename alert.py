d11f61fa28ccca2cc8773131ccd43cb2da50230b
AMMOUSAziz <aziz.ammous@insa-strasbourg.fr>
2026-07-14 14:09:48 +0200

    modifications of alert.py in order to adapt to the structure of the main

--- alert.py
+++ alert.py
@@ -1,20 +1,19 @@
-import Sensors.flameSensor as flame_sensor
-import Sensors.MQ_2 as MQ_2
 import LED.LED_Control as LED
 import Buzzer.Buzzer as Buzzer
 import Sensors.button as Button
 import time
-while True :
- while flame_sensor.getStateOfFlame() or MQ_2.getStateOfGas() : 
-    LED.setLedColor(1,0,0)
-    Buzzer.BuzzStart()
-
-    time.sleep(1)
-
-    LED.setLedColor(0,0,0)
-    Buzzer.BuzzStop()
+def main(StateOfFlame,StateOfGas): 
+ while True :
+  while StateOfFlame or StateOfGas : 
+     LED.setLedColor(1,0,0)
+     Buzzer.BuzzStart()
+ 
+     time.sleep(1)
+ 
+     LED.setLedColor(0,0,0)
+     Buzzer.BuzzStop()
     
-    time.sleep(0.5)
-    if Button.getStateOfButton: 
-        break
+     time.sleep(0.5)
+     if Button.getStateOfButton: 
+         break
 
